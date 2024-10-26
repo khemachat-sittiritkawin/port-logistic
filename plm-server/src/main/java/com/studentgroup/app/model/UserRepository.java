5656f29eabede22b942a025f151eb67d0fd6cff8
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-10-26 21:59:51 +0700

    แก้typeจากStringไปLong

--- plm-server/src/main/java/com/studentgroup/app/model/UserRepository.java
+++ plm-server/src/main/java/com/studentgroup/app/model/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 @Repository
-public interface UserRepository extends CrudRepository<EmployeeUser, String> {
+public interface UserRepository extends CrudRepository<EmployeeUser, Long> {
     EmployeeUser findByToken(String token);
     EmployeeUser findByUsername(String username);
 }
